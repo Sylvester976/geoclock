00923b5dd90965397c2178b17177bdb4814597bd
Sylvester976 <officialotienosylvester@gmail.com>
2025-07-01 11:50:45 +0300

    Setting.py updated, apps registered

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'attendance',
+    'worklocations',
+    'reporting',
 ]
 
 MIDDLEWARE = [
@@ -95,6 +99,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+# Static & Templates
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
