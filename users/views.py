e69607b54572e082ea658ce1fc9d9a98c475d0ea
Sylvester976 <officialotienosylvester@gmail.com>
2025-07-17 12:46:32 +0300

    login added. Simple one

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
 
-# Create your views here.
+def authlogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'status': 'success', 'message': 'Login successful.'})  # Replace with your desired URL name
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Invalid credentials.'})
+
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request.'})
